daa4ca9174135596cd23820a46b40e37c20156bc
AQ <nhaqq2003@gmail.com>
2024-07-23 20:02:01 +0700

    fix trang chu

--- lib/page/trangchu.dart
+++ lib/page/trangchu.dart
@@ -9,8 +9,6 @@ import 'package:flutter_doanlt/page/product_card1.dart';
 import 'package:flutter_doanlt/page/product_list_screen.dart';
 import 'package:flutter_doanlt/page/search.dart';
 
-import '../data/ApiService.dart';
-
 class HomePage extends StatefulWidget {
   final String token;
   final String userId;
@@ -22,43 +20,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String selectedCategory = '';
-  List<Shoe> shoes = [];
-  bool isLoading = true;
+  int _selectedIndex = 0;
   List<Widget> _screens = [];
-
-  void selectCategory(String category) {
-    setState(() {
-      selectedCategory = category;
-    });
-  }
 
   @override
   void initState() {
     super.initState();
-    _screens.addAll([
+    _screens = [
       HomePageContent(token: widget.token, userId: widget.userId),
       FavoriteScreen(),
       NotificationScreen(),
       AccountSettingScreen(token: widget.token, userId: widget.userId),
-    ]);
-    _loadProducts();
+    ];
   }
 
-  Future<void> _loadProducts() async {
-    ApiService apiService = ApiService();
-    try {
-      List<Shoe> fetchedShoes = await apiService.getShoes();
-      setState(() {
-        shoes = fetchedShoes;
-        isLoading = false;
-      });
-    } catch (e) {
-      print('Error loading products: $e');
-      setState(() {
-        isLoading = false;
-      });
-    }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
@@ -84,8 +63,7 @@ class _HomePageState extends State<HomePage> {
             padding: const EdgeInsets.fromLTRB(0, 16, 16, 0),
             child: InkWell(
               onTap: () {
-                // Navigator.push(context,
-                //     MaterialPageRoute(builder: (context) => CartScreen()));
+                // Handle cart button tap
               },
               splashColor: Color(0xFF6699CC),
               hoverColor: Color(0xFF6699CC),
@@ -101,22 +79,51 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: HomePageBody(token: widget.token, userId: widget.userId),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_active_outlined),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_outlined),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.brown,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
 
-class HomePageBody extends StatefulWidget {
+class HomePageContent extends StatefulWidget {
   final String token;
   final String userId;
 
-  HomePageBody({required this.token, required this.userId});
+  HomePageContent({required this.token, required this.userId});
 
   @override
-  _HomePageBodyState createState() => _HomePageBodyState();
+  _HomePageContentState createState() => _HomePageContentState();
 }
 
-class _HomePageBodyState extends State<HomePageBody> {
+class _HomePageContentState extends State<HomePageContent> {
   String selectedCategory = '';
   List<Shoe> products = [];
   bool isLoading = true;
@@ -148,14 +155,6 @@ class _HomePageBodyState extends State<HomePageBody> {
         isLoading = false;
       });
     }
-  }
-
-  void _navigateToDetailScreen(Shoe shoe) {
-    // Navigator.push(
-    //   context,
-    //   MaterialPageRoute(
-    //       builder: (context) => ProductDetailScreen(shoe: shoe)),
-    // );
   }
 
   @override
@@ -254,7 +253,7 @@ class _HomePageBodyState extends State<HomePageBody> {
                       SectionTitle(
                         title: 'Nổi Bật',
                         token: widget.token,
-                         userId: widget.userId,
+                        userId: widget.userId,
                         onViewAll: () {},
                       ),
                       SizedBox(height: 10.0),
@@ -275,7 +274,7 @@ class _HomePageBodyState extends State<HomePageBody> {
                       SectionTitle(
                         title: 'Sản Phẩm Mới',
                         token: widget.token,
-                         userId: widget.userId,
+                        userId: widget.userId,
                         onViewAll: () {},
                       ),
                       SizedBox(height: 10.0),
@@ -292,12 +291,73 @@ class _HomePageBodyState extends State<HomePageBody> {
                                   )
                                 : Center(child: Text('Không tìm thấy sản phẩm')),
                       ),
+                      SizedBox(height: 16.0),
+                      SectionTitle(
+                        title: 'Gợi ý',
+                        token: widget.token,
+                        userId: widget.userId,
+                        onViewAll: () {},
+                      ),
                     ],
+                  ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                  sliver: SliverGrid(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10.0,
+                      crossAxisSpacing: 10.0,
+                      childAspectRatio: 3 / 4,
+                    ),
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return ProductCard3(shoe: products[index]);
+                      },
+                      childCount: products.length,
+                    ),
                   ),
                 ),
               ],
             )
           : Center(child: Text('Không tìm thấy sản phẩm')),
+    );
+  }
+}
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+  final String userId;
+  final String token;
+  final VoidCallback onViewAll;
+
+  SectionTitle({required this.title, required this.token, required this.userId, required this.onViewAll});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProductListScreen(token: token, userId: userId),
+              ),
+            );
+          },
+          child: Text('Xem thêm', style: TextStyle(color: Colors.black54)),
+        ),
+      ],
     );
   }
 }
@@ -333,44 +393,6 @@ class CategoryButton extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class SectionTitle extends StatelessWidget {
-  final String title;
-  final String userId;
-  final String token;
-  final VoidCallback onViewAll;
-
-  SectionTitle({required this.title, required this.token, required this.userId,required this.onViewAll});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ProductListScreen(
-                token: token,
-                userId: userId,
-          )),
-            );
-          },
-          child: Text('Xem thêm', style: TextStyle(color: Colors.black54)),
-        ),
-      ],
     );
   }
 }
@@ -509,15 +531,99 @@ class ProductCard extends StatelessWidget {
   }
 }
 
-class HomePageContent extends StatelessWidget {
-  final String token;
-  final String userId;
+class ProductCard3 extends StatelessWidget {
+  final Shoe shoe;
 
-  HomePageContent({required this.token, required this.userId});
+  ProductCard3({required this.shoe});
 
   @override
   Widget build(BuildContext context) {
-    // Build your HomePage content here
-    return Container();
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+                  child: Image.asset(
+                    shoe.imageUrl,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 110.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'THỊNH HÀNH',
+                      style: TextStyle(
+                        color: Color(0xFF5B9EE1),
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      '\$${shoe.price}',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 10.0,
+            left: 10.0,
+            child: FavoriteButton(),
+          ),
+          Positioned(
+            bottom: 0.0,
+            right: 0.0,
+            child: InkWell(
+              onTap: () {
+                // handle tap event here
+              },
+              splashColor: Colors.white,
+              child: Container(
+                width: 50.0, // adjust the width
+                height: 50.0,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  ), // adjust the radius to your liking
+                ),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
